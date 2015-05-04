5e00f4f04c580c921954de255b94af22c5dabda3
Adam Murdoch <adam.murdoch@gradleware.com>
2015-05-04 12:34:09 +1000

    Some more detail in build environment report.

--- src/main/java/net/rubygrapefruit/gradle/gui/visualizations/BuildEnvironmentReport.java
+++ src/main/java/net/rubygrapefruit/gradle/gui/visualizations/BuildEnvironmentReport.java
@@ -10,7 +10,12 @@ public class BuildEnvironmentReport extends Report<BuildEnvironment> {
     @Override
     protected void render(BuildEnvironment environment, StructureVisitor tree) {
         tree.value("Gradle version", environment.getGradle().getGradleVersion());
+        tree.value("Gradle user home", environment.getGradle().getGradleUserHome());
         tree.value("Java home", environment.getJava().getJavaHome());
+        tree.collection("Requested JVM args", environment.getJava().getRequestedJvmArguments());
         tree.collection("JVM args", environment.getJava().getJvmArguments());
+        tree.collection("All JVM args", environment.getJava().getAllJvmArguments());
+        tree.map("Requested system properties", environment.getJava().getRequestedSystemProperties());
+        tree.map("System properties", environment.getJava().getSystemProperties());
     }
 }
