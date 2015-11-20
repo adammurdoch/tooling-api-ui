f600d181261c83afd89c4b98b1ab9ec2629c499e
Adam Murdoch <adam@gradle.com>
2015-11-20 14:28:57 +1100

    Updated Eclipse report to flatten projects into a single list and to show Java settings.

--- src/main/java/net/rubygrapefruit/gradle/gui/visualizations/EclipseModelReport.java
+++ src/main/java/net/rubygrapefruit/gradle/gui/visualizations/EclipseModelReport.java
@@ -2,40 +2,68 @@ package net.rubygrapefruit.gradle.gui.visualizations;
 
 import org.gradle.tooling.model.eclipse.EclipseProject;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 public class EclipseModelReport extends Report<EclipseProject> {
     public EclipseModelReport() {
         super("Eclipse model");
     }
 
     @Override
-    protected void render(EclipseProject project, StructureVisitor tree) {
-        tree.struct("Project", project.getName(), () -> {
-            tree.collection("Source directories", project.getSourceDirectories(), srcDir -> {
-                tree.struct("Path " + srcDir.getPath(), srcDir, entry -> {
-                    tree.value("Directory", srcDir.getDirectory());
+    protected void render(EclipseProject root, StructureVisitor tree) {
+        List<EclipseProject> projects = new ArrayList<>();
+        collect(root, projects);
+
+        for (EclipseProject project : projects) {
+            tree.struct("Project", project.getName(), () -> {
+                tree.collection("Source directories", project.getSourceDirectories(), srcDir -> {
+                    tree.struct("Path " + srcDir.getPath(), srcDir, entry -> {
+                        tree.value("Directory", srcDir.getDirectory());
+                    });
+                });
+                tree.collection("Linked resources", project.getLinkedResources(), linkedResource -> {
+                    tree.struct("Path " + linkedResource.getName(), linkedResource, entry -> {
+                        tree.value("Location", linkedResource.getLocation());
+                        tree.value("Location URI", linkedResource.getLocationUri());
+                        tree.value("Link type", linkedResource.getType());
+                    });
+                });
+                tree.collection("Dependencies", project.getProjectDependencies(), dependency -> {
+                    tree.struct(dependency.getTargetProject().getName(), dependency, eclipseDependency -> {
+                        tree.value("Exported", eclipseDependency.isExported());
+                    });
+                });
+                tree.collection("Classpath", project.getClasspath(), entry -> {
+                    String coords = String.format("Library %s:%s:%s", entry.getGradleModuleVersion().getGroup(),
+                            entry.getGradleModuleVersion().getName(), entry.getGradleModuleVersion().getVersion());
+                    tree.struct(coords, entry, externalDependency -> {
+                        tree.value("Exported", externalDependency.isExported());
+                        tree.value("File", externalDependency.getFile().getName());
+                    });
+                });
+                tree.collection("Natures", project.getProjectNatures(), nature -> {
+                    tree.value(nature.getId());
+                });
+                tree.collection("Builders", project.getBuildCommands(), buildCommand -> {
+                    tree.value(buildCommand.getName());
+                });
+                if (project.getJavaSourceSettings() != null) {
+                    tree.value("Java source level",
+                            project.getJavaSourceSettings().getSourceLanguageLevel().getVersion());
+                }
+                tree.collection("Children", project.getChildren(), child -> {
+                    tree.value("name", child.getName());
                 });
             });
-            tree.collection("Linked resources", project.getLinkedResources(), linkedResource -> {
-                tree.struct("Path " + linkedResource.getName(), linkedResource, entry -> {
-                    tree.value("Location", linkedResource.getLocation());
-                    tree.value("Location URI", linkedResource.getLocationUri());
-                    tree.value("Link type", linkedResource.getType());
-                });
-            });
-            tree.collection("Dependencies", project.getProjectDependencies(), dependency -> {
-                tree.struct(dependency.getTargetProject().getName(), dependency, eclipseDependency -> {
-                    tree.value("Exported", eclipseDependency.isExported());
-                });
-            });
-            tree.collection("Classpath", project.getClasspath(), entry -> {
-                String coords = String.format("Library %s:%s:%s", entry.getGradleModuleVersion().getGroup(),
-                        entry.getGradleModuleVersion().getName(), entry.getGradleModuleVersion().getVersion());
-                tree.struct(coords, entry, externalDependency -> {
-                    tree.value("Exported", externalDependency.isExported());
-                    tree.value("File", externalDependency.getFile().getName());
-                });
-            });
-            tree.collection("Children", project.getChildren(), child -> render(child, tree));
-        });
+        }
+    }
+
+    private void collect(EclipseProject project, Collection<EclipseProject> projects) {
+        projects.add(project);
+        for (EclipseProject child : project.getChildren()) {
+            collect(child, projects);
+        }
     }
 }
